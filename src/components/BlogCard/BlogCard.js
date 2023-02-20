d0e28c2c183066377c518f0e4d8b55832f7ab5db
alcun <alasdair@talkwell.world>
2023-02-20 17:56:00 +0000

    start card style

--- src/components/BlogCard/BlogCard.js
+++ src/components/BlogCard/BlogCard.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import styles from "@/styles/Home.module.css";
 
 const BlogCard = ({ postTitle, coverPhoto, slug }) => {
   return (
-    <div>
-      <Link href={`/posts/${slug}`}>
-        <Image width="500" height="500" src={coverPhoto.url} />
-      </Link>
+    <div className={styles.card}>
+        <Link className={styles.cardImgLink}href={`/posts/${slug}`}>
+          <Image fill src={coverPhoto.url} />
+        </Link>
       {postTitle}
     </div>
   );
