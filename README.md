b987ab24ea02f92e937f8b524c06e3394c9326e4
alcun <alasdair@talkwell.world>
2023-02-24 14:29:57 +0000

    add author to cards

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
 # Blog starter with Hygraph CMS
 
-start with nom run dev
+- start with npm run dev
+- aim is to create a base I can fork for other stuff then hone in on design for moonfrog
 
 ## TO DO
+
+### Components
+- Header
+- Footer
+
+### CSS / Design
+- General design
 - Responsive card grid
 - Responsive card size
-- Responsive truncate on card text 
 - General design for article 
 - Finish starter branch then make for moonfrog
-- favicon, page titles
-- host
+- favicon
+
+### Hosting
+- Deploy to vercel
--- src/components/BlogCard/BlogCard.js
+++ src/components/BlogCard/BlogCard.js
@@ -3,7 +3,14 @@ import Link from "next/link";
 import React from "react";
 import styles from "@/styles/Home.module.css";
 
-const BlogCard = ({ postTitle, coverPhoto, postText, slug }) => {
+const BlogCard = ({
+  postTitle,
+  coverPhoto,
+  postText,
+  slug,
+  datePublished,
+  author,
+}) => {
   return (
     <div className={styles.card}>
       <Link className={styles.card__link} href={`/posts/${slug}`}>
@@ -11,7 +18,18 @@ const BlogCard = ({ postTitle, coverPhoto, postText, slug }) => {
       </Link>
       <div className={styles.card__lower}>
         <h1 className={styles.card__lower__title}> {postTitle}</h1>
-        <span className={styles.card__lower__text}>{postText}</span>
+        <div className={styles.card__lower__info}>
+          <div className={styles.card__lower__info__author}>
+            <div>
+              <Image width={20} height={20} src={author.avatar.url} />
+            </div>
+            <div>{author.name}</div>
+          </div>
+          <div className={styles.card__lower__info__date}>{datePublished}</div>
+        </div>
+        <span className={styles.card__lower__text}>
+          {postText.replaceAll("\\n", " ")}
+        </span>
       </div>
     </div>
   );
